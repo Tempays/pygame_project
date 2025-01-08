0a955429c70fba1508a2283ec7d4031e563e8c22
Tempays <nazarenkotimofey2008@gmail.com>
2025-01-08 19:00:04 +0500

    Добавлен спрайт ромба; отрисовка нуждается в доработке

--- main.py
+++ main.py
@@ -8,6 +8,30 @@ clock = pygame.time.Clock()
 
 player = None
 
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+tile_width = tile_height = 50
+
+
+class RhombusSprite(pygame.sprite.Sprite):
+    def __init__(self, x, y, length, color):
+        super().__init__(tiles_group, all_sprites)
+        self.x = x * length
+        self.y = y * length
+        self.diagonal = (2 * length ** 2) ** 0.5
+        self.color = color
+        self.image = pygame.image
+
+    def draw(self, surface):
+        points = [
+            (self.x, self.y),
+            (self.x + self.diagonal / 2 + 20, self.y - self.diagonal / 2),
+            (self.x + self.diagonal + 40, self.y),
+            (self.x + self.diagonal / 2 + 20, self.y + self.diagonal / 2)
+        ]
+        pygame.draw.polygon(surface, self.color, points)
+        # Отрисовка ромба в зависимости от карты нуждается в доработке
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('../../Desktop/task/data', name)
@@ -25,8 +49,16 @@ def load_level(filename):
         return None
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
-    max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    return level_map
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                RhombusSprite(x, y, tile_width, pygame.Color('brown'))
+    return x, y, level
 
 
 def terminate():
@@ -38,21 +70,26 @@ def play_cycle():
     clock.tick(FPS)
     screen.fill('black')
     pygame.display.flip()
-    level = load_level('map.txt')
+    level = load_level(input())
     if level is None:
         print('Этого файла не существует')
         terminate()
+
     pygame.display.flip()
+    generate_level(level)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
 
         screen.fill('black')
+        for sprite in all_sprites:
+            sprite.draw(screen)
         pygame.display.flip()
 
 
 pygame.init()
-size = WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
+# size = WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
+size = WIDTH, HEIGHT = 500, 500
 screen = pygame.display.set_mode(size)
 play_cycle()
